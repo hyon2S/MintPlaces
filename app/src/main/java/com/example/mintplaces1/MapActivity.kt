1d397ffa934a72da4f201c8a679fd93e2a97c3b4
hyon2S <hhsim123@gmail.com>
2020-12-07 19:29:54 +0900

    이름 변경 MainActivity -> MapActivity activity_main.xml -> activity_map.xml

--- app/src/main/java/com/example/mintplaces1/MapActivity.kt
+++ app/src/main/java/com/example/mintplaces1/MapActivity.kt
@@ -3,9 +3,9 @@ package com.example.mintplaces1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_map)
     }
 }

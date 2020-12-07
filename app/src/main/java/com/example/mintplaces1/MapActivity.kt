ec7d83019b974b406963195c2353a4d817926d8d
hyon2S <hhsim123@gmail.com>
2020-12-08 01:03:29 +0900

    위치 정보 접근 권한 요청

--- app/src/main/java/com/example/mintplaces1/MapActivity.kt
+++ app/src/main/java/com/example/mintplaces1/MapActivity.kt
@@ -2,11 +2,42 @@ package com.example.mintplaces1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
+import androidx.activity.result.registerForActivityResult
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var map: GoogleMap
+
+    // https://developer.android.com/training/permissions/requesting
+    // https://pluu.github.io/blog/android/2020/05/01/migation-activity-result/
+    val locationAccessPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions(), LOCATION_ACCESS_PERMISSION) {
+                Log.d(TAG, "registerForActivityResult")
+                // 권한 모두 허용이면 true, 하나라도 미허용이면 false
+                var isGranted: Boolean = true
+                for (permission in it) {
+                    if (permission.value == false) {
+                        isGranted = false
+                        break
+                    }
+                }
+
+                when (isGranted) {
+                    true -> {
+                        Log.d(TAG, "권한 허용")
+                        // isMyLocationEnabled를 true로 세팅
+                        // 현재 위치 얻어오기
+                    }
+                    false -> {
+                        Log.d(TAG, "권한 미허용")
+                        // 위치 얻어올 수 없다고 메시지 띄우기
+                    }
+                }
+            }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +59,8 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         map.moveCamera(cameraUpdate)
 
         // 현재 위치 받아올 수 있으면 받아와서 카메라 현재 위치로 이동시킴.
+        Log.d(TAG, "권한요청")
+        locationAccessPermissionLauncher.launch()
     }
 
     // 지도 초기 세팅
@@ -45,9 +78,15 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     companion object {
+        const val TAG = "MyMapLog"
         // 지도 처음 시작 위치를 서울시청으로 설정.
         val SEOUL_CITY_HALL_LATLNG: LatLng = LatLng(37.566669, 126.978406)
         val DEFAULT_CAMERA_ZOOM: Float = 16.0f
+
+        val LOCATION_ACCESS_PERMISSION = arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
     }
 
 }

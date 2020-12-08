d28d5eda088913510be40f0c10f6449c7f95dd68
hyon2S <hhsim123@gmail.com>
2020-12-08 13:07:30 +0900

    현재 사용자의 위치를 주기적으로 가져옴

--- app/src/main/java/com/example/mintplaces1/MapActivity.kt
+++ app/src/main/java/com/example/mintplaces1/MapActivity.kt
@@ -1,16 +1,33 @@
 package com.example.mintplaces1
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.IntentSender
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
 import androidx.activity.result.registerForActivityResult
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.Task
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var map: GoogleMap
+    // 현재 위치 추적
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    private var currentLocation: Location? = null // 위치 추적 처음 시작할때는 null, 그 외에는 계속 새로 얻은 위치로 업데이트 시켜줌.
+    private val locationRequest = LocationRequest.create().apply {
+        interval = 5000
+        fastestInterval = 5000
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
 
     // 위치 접근 권한
     // https://developer.android.com/training/permissions/requesting
@@ -27,11 +44,13 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                     }
                 }
 
+                @SuppressLint("MissingPermission")
                 when (isGranted) {
                     true -> {
                         Log.d(TAG, "권한 허용")
-                        // isMyLocationEnabled를 true로 세팅
+                        map.isMyLocationEnabled = true
                         // 현재 위치 얻어오기
+                        startLocationUpdates()
                     }
                     false -> {
                         Log.d(TAG, "권한 미허용")
@@ -43,6 +62,29 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        // https://developer.android.com/training/location/request-updates#callback
+        // 주기적으로 현재 위치를 gps에서 받아와서 전역변수 currentLocation에 업데이트하게 함.
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                Log.d(TAG, "onLocationResult()")
+                locationResult ?: return
+                for (location in locationResult.locations){
+                    // 그나저나 location이 null일수도있나?
+                    if (currentLocation == null) {
+                        // 맨 처음에 기본 위치에서 시작할 때는 현재 위치로 카메라 이동하고 시작함.
+                        val currentLatLng = LatLng(location.latitude, location.longitude)
+                        val cameraUpdate: CameraUpdate = CameraUpdateFactory.newLatLng(currentLatLng)
+                        map.moveCamera(cameraUpdate)
+                    }
+                    // 위치가 null이 아닌 경우는 위치 업데이트만 하고 굳이 그 위치로 이동은 안 함.
+                    // 주기적으로 강제로 그 위치로 이동시키면 다른데 보다가 움직여서 화날수도 있음...
+                    currentLocation = location
+                }
+            }
+        }
+
         // 지도 본체 세팅
         val mapFragment = supportFragmentManager.findFragmentById(R.id.frg_map) as SupportMapFragment?
                 ?: SupportMapFragment.newInstance().also {
@@ -64,6 +106,53 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         locationAccessPermissionLauncher.launch()
     }
 
+    // https://developer.android.com/training/location/change-location-settings#get-settings
+    // https://developer.android.com/training/location/change-location-settings#prompt
+    // 현재 사용자의 위치 정보를 받아오는 세팅
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+        task.addOnSuccessListener {
+            Log.d(TAG, "위치 설정 이상 없음")
+            // 인터넷 연결 확인 필요
+            // 위치 정보 받아오기 시작
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+        }
+        task.addOnFailureListener {
+            Log.d(TAG, "위치 설정 실패: ${it.message}")
+            if (it is ResolvableApiException){
+                try {
+                    it.startResolutionForResult(this, GPS_REQ_CODE)
+                    // ActivityResultLauncher를 사용하는 방법도 있나..?
+                    // 결과 처리는 onActivityResult()에서...
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.d(TAG, "onActivityResult()")
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            GPS_REQ_CODE -> {
+                Log.d(TAG, "resultCode: ${resultCode}")
+                if (resultCode == RESULT_OK) {
+                    // 인터넷 연결 확인 필요
+                    // 위치 정보 받아오기 한 번 더 도전..
+                    fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+                } else {
+                    // 위치 정보에 접근할 수 없으니 걍 마지막 접속 위치로 카메라 이동
+                }
+            }
+        }
+    }
+
     // 지도 초기 세팅
     private fun setMap() {
         map.apply {
@@ -83,6 +172,8 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         // 지도 처음 시작 위치를 서울시청으로 설정.
         val SEOUL_CITY_HALL_LATLNG: LatLng = LatLng(37.566669, 126.978406)
         val DEFAULT_CAMERA_ZOOM: Float = 16.0f
+
+        private val GPS_REQ_CODE: Int = 100
 
         val LOCATION_ACCESS_PERMISSION = arrayOf(
                 android.Manifest.permission.ACCESS_COARSE_LOCATION,

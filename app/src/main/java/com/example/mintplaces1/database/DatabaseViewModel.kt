e38c291f1f570d74638a37572c31e216e3fcadc7
hyon2S <hhsim123@gmail.com>
2020-12-16 17:39:50 +0900

    변수명 변경

--- app/src/main/java/com/example/mintplaces1/database/DatabaseViewModel.kt
+++ app/src/main/java/com/example/mintplaces1/database/DatabaseViewModel.kt
@@ -7,9 +7,9 @@ import com.google.android.gms.maps.model.LatLng
 // 민트 음식을 파는 매장 정보 등을 Firebase에 저장하는 기능
 class DatabaseViewModel: ViewModel() {
     // 등록할 매장 정보를 보관함.
-    var markerPlaceInfo: PlaceInfo? = null
+    var placeInfo: PlaceInfo? = null
 
     fun setPlace(name: String, latLng: LatLng, address: String) {
-        markerPlaceInfo = PlaceInfo(name, latLng, address)
+        placeInfo = PlaceInfo(name, latLng, address)
     }
 }

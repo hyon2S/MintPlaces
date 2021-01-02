32d4228ccfabf1d330de5af451aa8999e2f78d17
hyon2S <hhsim123@gmail.com>
2021-01-02 18:35:56 +0900

    닉네임변경 시 예외적인 상황 메시지 띄우기

--- app/src/main/java/com/example/mintplaces1/user/UsernameActivity.kt
+++ app/src/main/java/com/example/mintplaces1/user/UsernameActivity.kt
@@ -5,14 +5,28 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.example.mintplaces1.R
+import com.example.mintplaces1.network.NetworkConnectionCheckAdapter
+import com.example.mintplaces1.network.NetworkConnectionChecker
 import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.android.synthetic.main.activity_username.*
 
 // 사용자의 닉네임(displayName)을 변경하는 액티비티
-class UsernameActivity : AppCompatActivity() {
+class UsernameActivity : AppCompatActivity(), NetworkConnectionCheckAdapter {
+    // 네트워크 연결 상태 확인
+    override lateinit var networkConnectionChecker: NetworkConnectionChecker
+    // 호출했을 시점에 네트워크 연결이 끊겨있으면 메시지 띄움.
+    override fun notifyNetworkConnection(): Boolean {
+        val isNetworkConnected = networkConnectionChecker.isNetworkConnected()
+        if (!isNetworkConnected)
+            Toast.makeText(this, getString(R.string.request_network_connection), Toast.LENGTH_SHORT).show()
+        return isNetworkConnected
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_username)
+
+        networkConnectionChecker = NetworkConnectionChecker(baseContext)
 
         // 사용자가 null인지 확인하고 null이면 종료해야되는거 아님?
 
@@ -32,6 +46,7 @@ class UsernameActivity : AppCompatActivity() {
         val username: String = et_username.text.toString().trim()
         if (username.isEmpty()) {
             // 이름 입력해달라고 메시지 띄움
+            Toast.makeText(this, getString(R.string.require_username_message), Toast.LENGTH_SHORT).show()
             return
         }
         val user = FirebaseUtil.getUser()
@@ -47,6 +62,8 @@ class UsernameActivity : AppCompatActivity() {
                     finish()
                 } else {
                     Log.d(TAG, "이름 변경 실패: ${it.exception}")
+                    notifyNetworkConnection()
+                    Toast.makeText(this, getString(R.string.fail_message_change_username), Toast.LENGTH_SHORT).show()
                 }
             }
         } else {
